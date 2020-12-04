78a7e2bdeacc5e3f729205ed7cf08fdfb1ad8596
Kushal Khangaonkar <kushaltk@berkeley.edu>
2020-12-04 10:57:15 -0800

    hi

--- prepare_submission.py
+++ prepare_submission.py
@@ -15,5 +15,20 @@ def main():
             submission[input_path] = output
     with open(submission_name, 'w') as f:
         f.write(json.dumps(submission))
-
+    for input_path in os.listdir("inputs/medium"):
+        graph_name = input_path.split('.')[0]
+        output_file = f'{outputs_dir}/{graph_name}.out'
+        if os.path.exists(output_file) and validate_file(output_file):
+            output = open(f'{outputs_dir}/{graph_name}.out').read()
+            submission[input_path] = output
+    with open(submission_name, 'w') as f:
+        f.write(json.dumps(submission))
+    for input_path in os.listdir("inputs/medium/khoa"):
+        graph_name = input_path.split('.')[0]
+        output_file = f'{outputs_dir}/{graph_name}.out'
+        if os.path.exists(output_file) and validate_file(output_file):
+            output = open(f'{outputs_dir}/{graph_name}.out').read()
+            submission[input_path] = output
+    with open(submission_name, 'w') as f:
+        f.write(json.dumps(submission))
 main()
