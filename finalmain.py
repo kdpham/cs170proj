2eddbd2399cbe912a881f0e3bbdc0a72f943a1e6
khoa <khoadp@berkeley.edu>
2020-12-05 02:46:20 +0000

    done

--- finalmain.py
+++ finalmain.py
@@ -65,7 +65,7 @@ def idkman(filename):
             model.add_constraint(model.sum(x[(i, j)] for i in range(k)) == 1)
 
         model.maximize(model.sum(happiness_rooms[i] for i in range(k)))
-        model.set_time_limit(30)
+        model.set_time_limit(60)
         solution = model.solve()
         try:
             if (solution.get_objective_value() > maxScore):
